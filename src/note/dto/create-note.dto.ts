5f8e9a0d34473a81fdf852d7aa6c9c87b4e1cf7f
itsayush30 <itsayush30@gmail.com>
2024-03-13 23:28:35 +0530

    Added data validation and error handling to create not API

--- src/note/dto/create-note.dto.ts
+++ src/note/dto/create-note.dto.ts
@@ -1,9 +1,21 @@
+import { IsNotEmpty, IsString } from "class-validator";
 import { Mood } from "../schemas/note.schema";
 
 
 export class CreateNoteDto{
+
+    @IsNotEmpty()
+    @IsString()
     readonly title:string;
+
+
+    @IsNotEmpty()
+    @IsString()
     readonly author: string;
+
+
+    @IsNotEmpty()
+    @IsString()
     readonly note: string;
     readonly mood: Mood
     readonly specialnote: string;
